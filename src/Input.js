c0e09279ac0ed83104a9994c3c0e9f905f0a04f8
William Tange <will@tan.ge>
2024-02-24 23:34:47 +0100

    Set input value on select of option

--- src/Input.js
+++ src/Input.js
@@ -2,9 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 
 export function Input(props) {
   const [results, setResults] = useState(null);
+  const input = useRef(null);
   return (
     <>
       <input
+        ref={input}
         className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         placeholder={props.placeholder}
         onChange={async (e) => {
@@ -30,6 +32,7 @@ export function Input(props) {
                         if (props.onSelect) {
                           props.onSelect(r, k);
                         }
+                        input.current.value = r;
                         setResults(null);
                       }}
                     >
@@ -44,7 +47,6 @@ export function Input(props) {
           </ul>
         )}
       </div>
-      <p>Lorem ipsum</p>
     </>
   );
 }
